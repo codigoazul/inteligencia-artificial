774cadd69fec3afbd1d4cbee45f46411e8bdaf3d
Vicente Mateus Konageski Ferreira <110827058+codigoazul@users.noreply.github.com>
2024-06-10 14:17:31 +0000

    aula 3

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const caixaPrincipal= document.querySelector(".caixa-principal");
 const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaalternativas= document.querySelector(".caixa-alternativas");
+const caixaresultado= document.querySelector(".caixa-resultado");
+const textoresultado= document.querySelector(".testto resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
